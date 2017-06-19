b2e74e8ab4b1ef53dde591a1bd4951004ce50efa
Alex Kulagin <alexkulagin@users.noreply.github.com>
2017-06-19 21:36:14 +0300

    update
    Обновлен ax.toolkit.js — добавлена функция "_fix", исправляющая баги Axure API.

--- ax.toolkit.js
+++ ax.toolkit.js
@@ -45,8 +45,8 @@
 
 		/**
 		 * "DOMContentLoaded" и "onload" предоставляют возможность вызова функции до
-		 *  и после отработки события из прототипа "OnPageLoad" ("OnPageLoad" срабатывает
-		 *  сразу после загрузки страницы целиком)
+		 *  и после отработки события "OnPageLoad" ("OnPageLoad" — события из прототипа
+		 *  срабатывает сразу после загрузки страницы целиком)
 		 */
 		
 		_w.onload = _afterPageOnLoad;
@@ -55,12 +55,78 @@
 
 		const _beforePageOnLoad = function ()
 		{
-			console.log( 'Before OnPageLoad' );
+			_fix();
+			// console.log( 'Before OnPageLoad' );
 		};
 
 		const _afterPageOnLoad =  function ()
 		{
-			console.log( 'After OnPageLoad' );
+			// console.log( 'After OnPageLoad' );
+		};
+
+
+		/**
+		 * Исправляет баги Axure API
+		 * 
+		 * • getGlobalVariable
+		 * • value
+		 */
+		
+		const _fix = function ()
+		{
+			_private.public.getGlobalVariable = _private.getGlobalVariable = function(name) {
+				return _private.globalVariableProvider.getVariableValue(name);
+			};
+
+			_private.public.fn.value = function()
+			{
+				if (arguments[0] == undefined)
+				{
+					var firstId = this.getElementIds()[0];
+
+					if(!firstId) return undefined;
+
+					var widgetType = _private.getTypeFromElementId(firstId);
+
+					if (_private.public.fn.IsComboBox(widgetType) || _private.public.fn.IsListBox(widgetType)) {
+						return $('#' + firstId + ' :selected').text();
+					} else if (_private.public.fn.IsCheckBox(widgetType) || _private.public.fn.IsRadioButton(widgetType)) {
+						return $('#' + firstId + '_input').is(':checked');
+					} else if (_private.public.fn.IsTextBox(widgetType)) {
+						return $('#' + firstId + '_input').val();
+					} else {
+						return this.jQuery().first().val();
+					}
+				} else {
+					var elementIds = this.getElementIds();
+
+					for(var i = 0; i < elementIds.length; i++)
+					{
+						var widgetType = _private.getTypeFromElementId(elementIds[i]);
+
+						var elementIdQuery = $('#' + elementIds[i]);
+
+						if (_private.public.fn.IsCheckBox(widgetType) || _private.public.fn.IsRadioButton(widgetType))
+						{
+							if(arguments[0] == true) {
+								elementIdQuery.attr('checked', true);
+							} else if(arguments[0] == false) {
+								elementIdQuery.removeAttr('checked');
+							}
+						} else if (_private.public.fn.IsTextBox(widgetType)) {
+							
+							$('#' + elementIdQuery[0].id + '_input').val(arguments[0]);
+
+						} else {
+							elementIdQuery.val(arguments[0]);
+						}
+					}
+
+					return this;
+				}
+			};
+
+			return true;
 		};
 
 	})();
